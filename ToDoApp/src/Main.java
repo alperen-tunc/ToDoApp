208b52b5584c94bebae6e8c73a43b66cb453ed2b
alperentuncc <115324004+alperen-tunc@users.noreply.github.com>
2022-12-18 21:32:34 +0100

    update_fromHome_18.12

---
+++ ToDoApp/src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/Login.fxml")));
+        primaryStage.setTitle("My New");
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
+    }
+}
