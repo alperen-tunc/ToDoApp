723d27857091c4cab966ccaeb3b83607456db600
alperentuncc <115324004+alperen-tunc@users.noreply.github.com>
2022-12-18 21:58:56 +0100

    update_fromHome_18.12

--- ToDoApp/src/controller/SignupController.java
+++ ToDoApp/src/controller/SignupController.java
@@ -1,5 +1,51 @@
 package controller;
 
-public class SignupController
-{
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField signUpFirstName;
+
+    @FXML
+    private TextField signUpLastName;
+
+    @FXML
+    private TextField signUpUserName;
+
+    @FXML
+    private TextField signUpLocation;
+
+    @FXML
+    private CheckBox signUpCheckBoxMale;
+
+    @FXML
+    private CheckBox signUpCheckBoxFemale;
+
+    @FXML
+    private CheckBox signUpCheckBoxDiverse;
+
+    @FXML
+    private TextField signUpPassword;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize()
+    {
+
+
+    }
 }
